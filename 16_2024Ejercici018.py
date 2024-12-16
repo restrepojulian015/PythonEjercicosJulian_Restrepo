49338a4978163e89df08f542435061acfaed4f57
Julian Andres Restrepo <164690643+restrepojulian015@users.noreply.github.com>
2024-12-16 14:00:47 -0500

    Python_04

---
+++ 16_2024Ejercici018.py
@@ -0,0 +1,50 @@
+
+list =[10,2]
+print(list[0])
+
+list[1] = 3
+print(list)
+
+list.append(3)
+print(list[2])
+
+print(len(list))
+
+newlist = [1,2]
+
+newlist.insert(1,5)
+print(newlist)
+
+list.extend(newlist)
+print(list)
+
+list.append(1)
+print(list)
+
+list.remove(1)
+print(list)
+
+list.pop(1)
+print(list)
+
+print(list.count(3))
+
+listal = [3,5,4,1,2]
+listal.sort(reverse = True)
+print(listal)
+
+list.reverse
+print(list)
+
+nuevalisa = list
+nuevalisa.append(5)
+print(nuevalisa,list)
+
+list2 = ["Manzana", list]
+print(list2)
+
+list.clear()
+nuevalisa.clear()
+list2.clear()
+listal.clear
+print(list,list2,listal)
---
+++ 16_2024Ejercicio17.py
@@ -0,0 +1,14 @@
+#
+'''
+iiouhuhuohouhouhouhouhou
+'''
+
+animal = "Perro"
+
+def miAnimal():
+    #global animal
+    animal = "gato"
+
+miAnimal()
+
+print(f"El animal es: {animal}")
